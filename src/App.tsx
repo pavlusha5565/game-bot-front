900b84fa0e2a1bdcfabaa6cde96405682971bfe9
Pavel <pavlusha5565@gmail.com>
2023-01-07 22:42:32 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import AppRoutes from "./routes";
+import ErrorBoundary from "./routes/ErrorPage/ErrorPage";
+
+function App() {
+  return (
+    <div className="App">
+      <ErrorBoundary>
+        <AppRoutes />
+      </ErrorBoundary>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { StoreProvider } from "./stores/GlobalStoreContext";
+
+import "./index.scss";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <StoreProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StoreProvider>
+  </React.StrictMode>
+);
